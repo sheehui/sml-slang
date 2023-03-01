a191d7a8cea050e5e91cd3307ff18e0251eb5961
sheehui <tansheehui@gmail.com>
2023-03-01 21:15:08 +0800

    add unop binop test

--- tests/index.tests.ts
+++ tests/index.tests.ts
@@ -9,7 +9,7 @@ const options: Partial<IOptions> = {
     useSubst: false
 }
 
-describe ('testing', () => {
+describe ('literals', () => {
     test('Number literal expression', () => {
         const code : string = "5;"
         return runInContext(code, context, options).then(data => {
@@ -28,6 +28,87 @@ describe ('testing', () => {
         const code : string = "\"test string\";"
         return runInContext(code, context, options).then(data => {
             expect((data as Finished).value).toBe("test string")
+        }); 
+    })
+
+    test('String of boolean keyword', () => {
+        const code : string = "val bool = \"true\";"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe("true")
+        }); 
+    })
+
+    test('String of numbers', () => {
+        const code : string = "val num = \"947563\";"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe("947563")
+        }); 
+    })
+
+    test('String of nil', () => {
+        const code : string = "val n = \"nil\";"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe("nil")
+        }); 
+    })
+})
+
+describe('unop expressions', () => {
+    test('negation', () => {
+        const code : string = "val num = 234; ~num;"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe(-234)
+        }); 
+    })
+
+    test('not', () => {
+        const code : string = "val bool = true; not bool;"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe(false)
+        }); 
+    })
+})
+
+describe('binop', () => {
+    test('minus', () => {
+        const code : string = "3 - 4;"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe(-1)
+        }); 
+    })
+
+    test('plus', () => {
+        const code : string = "~1 + 90;"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe(89)
+        }); 
+    })
+
+    test('divide', () => {
+        const code : string = "20 div 3;"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe(6)
+        }); 
+    })
+
+    test('multiply', () => {
+        const code : string = "3 * 5;"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe(15)
+        }); 
+    })
+
+    test('concat', () => {
+        const code : string = "\"hello\" ^ \" world\";"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe("hello world")
+        }); 
+    })
+
+    test('modulo', () => {
+        const code : string = "10 mod 3;"
+        return runInContext(code, context, options).then(data => {
+            expect((data as Finished).value).toBe(1)
         }); 
     })
 })
